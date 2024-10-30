bb47e9073d3cd94c180f6d629236d4d955561765
Caio <caiohbda777@gmail.com>
2024-10-30 14:12:09 -0300

    adaptando estrutura

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,18 +1,10 @@
 import axios from "axios";
+import { IUser } from "../interfaces/IUser";
 
 const API_URL = "http://localhost:8080";
 
-interface User {
-  fullName: string;
-  username: string;
-  email: string;
-  password: string;
-  phoneNumber: string;
-  image?: string;
-}
-
 const UserService = {
-  createUser: async (userData: User) => {
+  createUser: async (userData: IUser) => {
     try {
       const response = await axios.post(`${API_URL}/users`, userData, {
         headers: {
