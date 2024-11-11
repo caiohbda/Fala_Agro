f27ab02d2526f71aaf5826c4375eb8963882c3ba
Caio <caiohbda777@gmail.com>
2024-11-11 15:33:11 -0300

    botões para direcionar para criações

--- src/pages/EventPage/index.tsx
+++ src/pages/EventPage/index.tsx
@@ -1,13 +1,17 @@
 import Header from "../../components/Header";
 import Card from "../../components/Card";
+import "./style.css";
 
 const EventPage = () => {
   return (
     <div>
       <Header />
+      <div className="button-container">
+        <button className="create-event-button">Criar evento</button>
+      </div>
       <main className="feed">
-        {[1, 2, 3, 4, 5, 6].map(() => (
-          <Card />
+        {[1, 2, 3, 4, 5, 6].map((_, index) => (
+          <Card key={index} />
         ))}
       </main>
     </div>
--- src/pages/NewsPage/index.tsx
+++ src/pages/NewsPage/index.tsx
@@ -1,10 +1,14 @@
 import Header from "../../components/Header";
 import Card from "../../components/Card";
+import "./style.css";
 
 const NewsPage = () => {
   return (
     <div>
       <Header />
+      <div className="button-container">
+        <button className="create-event-button">Publicar Notícia</button>
+      </div>
       <main className="feed">
         {[1, 2, 3, 4, 5, 6].map(() => (
           <Card />
