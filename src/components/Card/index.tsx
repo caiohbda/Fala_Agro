2878568263449f65139ff88533b540bd9f9718a1
Caio <caiohbda777@gmail.com>
2024-11-19 09:59:55 -0300

    cards ajustados

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React from "react";
 import "./style.css";
 
 interface CardProps {
-  image?: string,
-  title?: string,
+  image?: string;
+  title?: string;
 }
 
-const Card: React.FC<CardProps> = ({
-  image,
-  title
-}) => {
-    return(
-        <div className="feed-item">
-          <div className="feed-container">
-            <h2>{title}</h2>
-          </div>
-          <img src={image} alt="imagem da noticia" />
-        </div>
-    )
-}
+const Card: React.FC<CardProps> = ({ image, title }) => {
+  return (
+    <div className="feed-item">
+      <img src={image} alt="imagem da noticia" />
+      <div className="feed-container">
+        <h2>{title}</h2>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
